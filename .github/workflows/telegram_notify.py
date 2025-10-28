711f59b74d4fb13d6b99086ff4ffbc44e0a66fd3
My name is 010595140 <zaycevnet5@mail.ru>
2025-10-28 09:35:21 +0700

    Fix deploy configuration and GitHub Pages setup

--- .github/workflows/telegram_notify.py
+++ .github/workflows/telegram_notify.py
@@ -5,7 +5,7 @@ import requests
 TOKEN = os.getenv("TELEGRAM_TOKEN")
 CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
-STATUS = os.getenv("PIPELINE_STATUS", "unknown")
+STATUS = os.getenv("PIPELINE_STATUS", "unknown") # Получаем статус пайплайна из переменной окружения
 
 # Формируем сообщение в зависимости от результата
 if STATUS == "success":
@@ -20,12 +20,12 @@ url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
 
 # Отправляем сообщение
 try:
-    response = requests.post(
-        url,
-        data={"chat_id": CHAT_ID, "text": message},
-        timeout=10
+    response = requests.post( #Выполняет HTTP-запрос типа POST на сгенерированный url. Это и есть сам факт отправки сообщения.
+        url, #URL для отправки сообщения
+        data={"chat_id": CHAT_ID, "text": message}, #Данные, которые отправляются в запросе. В данном случае это идентификатор чата и текст сообщения.
+        timeout=10 #Максимальное время ожидания ответа от сервера (в секундах).
     )
-    if response.status_code != 200:
-        print(f"Ошибка при отправке: {response.text}")
-except requests.RequestException as e:
-    print(f"Не удалось отправить сообщение: {e}")
+    if response.status_code != 200: #Проверяет, успешен ли запрос (код 200 означает успех).
+        print(f"Ошибка при отправке: {response.text}") #Если запрос не успешен, выводит текст ошибки.
+except requests.RequestException as e: 
+    print(f"Не удалось отправить сообщение: {e}") 
